64bd3d827c7fa505f979af3305d569b2c6dfb9d4
Hadi Mazloum <hadimazloum@Hadis-MacBook-Pro.local>
2026-04-24 21:09:19 +1000

    Add Canberra Philharmonic Society ticket URL

--- app/show/[id]/page.tsx
+++ app/show/[id]/page.tsx
@@ -86,6 +86,7 @@ function getTicketUrl(company: string, city: string, title: string, country: str
     'Koorliny Arts Centre': 'https://www.koorliny.com.au/',
     'Roleystone Theatre': 'https://www.roleystonetheatre.com.au/',
     'Free Rain Theatre Co': 'https://www.freeraintheatre.com/',
+    'Canberra Philharmonic Society': 'https://www.philo.org.au/',
     'Canberra Repertory Society': 'https://canberrarep.org.au/',
   }
   if (companyDirect[company]) return companyDirect[company]
